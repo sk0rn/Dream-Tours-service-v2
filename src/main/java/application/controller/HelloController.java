df0197e68ba6d2ff7cd5c7c2bf121f292bfd6c5a
Alex Adamov <alex.v.adamov@gmail.com>
2018-10-16 16:53:12 +0300

    some changes

--- src/main/java/application/controller/HelloController.java
+++ src/main/java/application/controller/HelloController.java
@@ -1,9 +1,9 @@
 package application.controller;
 
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 public class HelloController {
 
     @RequestMapping("/")
