5039a44d5215b29d1b376f461a92d561075cbb38
Alex Adamov <alex.v.adamov@gmail.com>
2018-10-17 16:35:43 +0300

    naked tours servlet added

---
+++ src/main/java/application/controller/UserController.java
@@ -0,0 +1,26 @@
+package application.controller;
+
+import application.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class UserController {
+
+    private final UserRepository userRepository;
+
+    @Autowired
+    public UserController(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @GetMapping("/profile")
+    public String tour(Model model
+                       //TODO после включения security
+            /*, @AuthenticationPrincipal User user*/) {
+
+        return "customer";
+    }
+}
--- src/main/java/application/repository/TourRepository.java
+++ src/main/java/application/repository/TourRepository.java
@@ -4,5 +4,5 @@ import application.domain.Tour;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface TourRepository extends JpaRepository<Tour, Long> {
-
+    Tour findById(Integer id);
 }
