1c5b44aa720ca73d17dd55f6d36856c84d3ad448
Brambrulet <brambrulet@gmail.com>
2018-10-17 23:35:50 +0300

    Services+Repositories

--- src/main/java/application/Application.java
+++ src/main/java/application/Application.java
@@ -19,7 +19,7 @@ public class Application {
     public CommandLineRunner commandLineRunner(ApplicationContext ctx) {
         return args -> {
             System.out.println("Let's inspect the beans provided by Spring Boot:");
-            System.out.println("no no no ... it's bad idea ... go out!");
+            System.out.println("no no no ... it's a bad idea ... go out!");
             if (true) return;
 
             String[] beanNames = ctx.getBeanDefinitionNames();
