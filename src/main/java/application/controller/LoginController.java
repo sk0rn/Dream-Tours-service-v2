99a1bd1107f35a125fbd0aa4c466d6eaf02c60d4
sk0rn <hyperthrone@gmail.com>
2018-10-23 23:41:43 +1100

    fix logout, refactoring security config & login controller

--- src/main/java/application/controller/LoginController.java
+++ src/main/java/application/controller/LoginController.java
@@ -2,30 +2,16 @@ package application.controller;
 
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
+
 
 @Controller
 public class LoginController {
-
-    @RequestMapping(value="/logout", method = RequestMethod.GET)
-    public String logoutPage (HttpServletRequest request,
-                              HttpServletResponse response,
-                              Model model) {
-        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
-        if (auth != null){
-            new SecurityContextLogoutHandler().logout(request, response, auth);
-            model.addAttribute("logout", true);
-        }
-        return "loginEvent";
-    }
 
     @RequestMapping("/login")
     public String getLogin(@RequestParam(value = "error", required = false) String error,
@@ -39,17 +25,20 @@ public class LoginController {
         return "loginEvent";
     }
 
-    @RequestMapping("/403")
-    public String error403(Model model) {
-        model.addAttribute("denied", true);
-        return "/loginEvent";
-    }
-
     @RequestMapping("/welcome")
     public String login(Model model, HttpServletRequest request) {
         // set user role to session
         Authentication auth = SecurityContextHolder.getContext().getAuthentication();
         request.getSession().setAttribute("role", auth.getAuthorities().toString());
         return "redirect:/tours";
+    }
+
+    @RequestMapping("/loginevents")
+    public String handleLoginEvents(@RequestParam(value = "logout", required = false) String logout,
+                                    @RequestParam(value = "denied", required = false) String denied,
+                                    Model model) {
+        model.addAttribute("logout", logout != null);
+        model.addAttribute("denied", denied != null);
+        return "loginEvent";
     }
 }
